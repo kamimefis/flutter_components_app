d9129b9d1bb4fc95638c4ed458ef744433ee20a4
kamimefis <48956064+kamimefis@users.noreply.github.com>
2022-03-11 14:52:44 -0300

    infinite scroll

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -8,7 +8,26 @@ class ListViewPage extends StatefulWidget {
 
 class _ListViewPageState extends State<ListViewPage> {
 
-  List<int> _numberList= [1,2,3,4,5];
+  //Create an object to control the scroll
+  ScrollController _scrollController = new ScrollController();
+
+  List<int> _numberList= [] /*[1,2,3,4,5]*/;
+  int _lastItem = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _addItemsToListView();
+
+    _scrollController.addListener(() {
+      //if the current position of the scroll is equal to the end of the screen:
+      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){
+        //add more items to the listview:
+        _addItemsToListView();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +39,7 @@ class _ListViewPageState extends State<ListViewPage> {
 
   Widget _createList() {
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _numberList.length,
       itemBuilder: (BuildContext context, int index){
 
@@ -31,6 +51,19 @@ class _ListViewPageState extends State<ListViewPage> {
         );
       }
     );
+  }
+
+
+  //method to add 10 images at the end of the image scroll
+  void _addItemsToListView() {
+    for (var i = 1; i < 10; i++) {
+      _lastItem++;
+      _numberList.add(_lastItem);
+    }
+    setState(() {
+      
+    });
+
   }
 
 }
